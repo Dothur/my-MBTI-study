eb6148e6e2526e2ccd0378f9fba75aaa87cbc4cf
dothur <craz1000@naver.com>
2023-03-23 01:27:51 +0900

    QnA section 구현 (qBox, answerBox) answer 를 선택하면 다음 answerList 들이 생성됨

--- js/start.js
+++ js/start.js
@@ -1,6 +1,39 @@
 const mainBtn = document.querySelector("#mainBtn");
 const qna = document.querySelector("#qna");
 
+function changeAnswerList(qIndex) {
+  const preAnswerList = document.querySelectorAll(".answerList");
+  for (let i of preAnswerList) {
+    i.remove();
+    i.style.display = "none";
+  }
+  paintQnA(qIndex + 1);
+}
+
+function paintAnswer(answerText, qIndex) {
+  const answerBox = document.querySelector(".answerBox");
+  const answer = document.createElement("button");
+  answer.classList.add("answerList");
+  answerBox.appendChild(answer);
+  answer.innerHTML = answerText;
+
+  answer.addEventListener(
+    "click",
+    () => {
+      changeAnswerList(qIndex);
+    },
+    false
+  );
+}
+
+function paintQnA(qIndex) {
+  const questions = document.querySelector(".qBox");
+  questions.innerHTML = qnaList[qIndex].q;
+  for (let i in qnaList[qIndex].a) {
+    paintAnswer(qnaList[qIndex].a[i].answer, qIndex);
+  }
+}
+
 function handleStartButton() {
   main.style.WebkitAnimation = "fadeOut 0.5s";
   main.style.Animation = "fadeOut 0.5s";
@@ -11,6 +44,8 @@ function handleStartButton() {
       main.style.display = "none";
       qna.style.display = "block";
     }, 200);
+    const qIndex = 0;
+    paintQnA(qIndex);
   }, 200);
 }
 mainBtn.addEventListener("click", handleStartButton);
