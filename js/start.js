d47bd00a3d7a264b1047336065d3c2f8d7035174
dothur <craz1000@naver.com>
2023-03-22 23:53:18 +0900

    시작하기를 누르고 fadeIn, fadeOut 효과를 추가하였다.

--- js/start.js
+++ js/start.js
@@ -1,7 +1,16 @@
-const main = document.querySelector("#main");
+const mainBtn = document.querySelector("#mainBtn");
 const qna = document.querySelector("#qna");
 
-function begin() {
-  main.style.display = "none";
-  qna.style.display = "block";
+function handleStartButton() {
+  main.style.WebkitAnimation = "fadeOut 0.5s";
+  main.style.Animation = "fadeOut 0.5s";
+  setTimeout(() => {
+    qna.style.WebkitAnimation = "fadeIn 0.5s";
+    qna.style.Animation = "fadeIn 0.5s";
+    setTimeout(() => {
+      main.style.display = "none";
+      qna.style.display = "block";
+    }, 200);
+  }, 200);
 }
+mainBtn.addEventListener("click", handleStartButton);
